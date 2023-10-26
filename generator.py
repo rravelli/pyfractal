be8fa92c70a8d6d95a1139e26be962030edef75b
Manon Coutier <manon-c.coutier@wanadoo.fr>
2023-10-26 08:53:42 +0200

    Generators documented

--- generator.py
+++ generator.py
@@ -1,18 +1,56 @@
 def sequence(z, c) -> complex:
-    """Générateur des éléments de la suite $z_{n+1}=z_n^2+c$
-    c.f. Chapitre 2"""
+    """Generator of the elements of the sequence $z_{n+1}=z_n^2+c$
+
+    Parameters
+    ----------
+    z : complex
+        value of the precedent iteration of the sequence
+    c : complex
+        constant complex added
+
+    Yield   
+    -----
+    complex
+        next value of the sequence
+    """
     while True:
         yield z
         z = z**2 + c
 
 
-def suite_mandelbrot(c) -> complex:
-    """_summary_
+def mandelbrot_sequence(c:complex) -> complex:
+    """
+    Generator of the elements of the elements of the Mandelbrot sequence.
+    Mandelbrot sequence is $z_{n+1}=z_n^2+c$ with $z_{0}=0$
 
-    Args:
-        c (_type_): _description_
+    Parameters
+    ----------
+    c : complex
+        c in the sequence $z_{n+1}=z_n^2+c$
 
-    Returns:
-        complex: _description_
+    Yield   
+    -----
+    complex
+        next value of the Mandelbrot sequence
     """
     return sequence(0, c)
+
+def julia_sequence(z:complex,c=complex)-> complex:
+    """
+    Generator of the elements of the elements of the Julia sequence.
+    Julia sequence is $z_{n+1}=z_n^2+c$ with $z_{0}!=0$
+
+    Parameters
+    ----------
+    z : complex
+        $z_{0}$ in the sequence $z_{n+1}=z_n^2+c$
+    
+    c : complex
+        c in the sequence $z_{n+1}=z_n^2+c$
+
+    Yield   
+    -----
+    complex
+        next value of the Julia sequence
+    """
+    return sequence(z,c)
